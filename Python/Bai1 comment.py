b9f27da05a85e5bb706127cf1126c61196067388
HienTai <tai.nguyenraphael@hcmut.edu.vn>
2021-09-07 14:37:19 +0700

    commit

--- Python/Bai1 comment.py
+++ Python/Bai1 comment.py
@@ -1,5 +1,8 @@
-#in dong chu Hello World
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+#in dòng chữ "hello world"
 print("Hello World")
 
-#in dong chu day la chuoi comment
-print("day la chuoi comment")
+#in dòng chữ "đây là chuỗi comment"
+print("đây là chuỗi comment")
