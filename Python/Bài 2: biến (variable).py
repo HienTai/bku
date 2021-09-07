c8a24e9c8bc2e3cb3c9c4d0e7cea16519b05ffa2
HienTai <tai.nguyenraphael@hcmut.edu.vn>
2021-09-07 15:04:46 +0700

    commit

---
+++ Python/Bài 2: biến (variable).py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+h = 19082003
+t = 11082003
+s = h+t
+print(s)
+
+#tên biến bao gồm số, chữ và dấu _
+#phân biệt đc chữ hoa thường
+
+#khởi tạo nhiều biến
+hi, ti = "hiền", "tài"
+print(hi)
+print(ti)
+
+#kiểm tra kiểu dữ liệu của biến đã khởi tạo
+#Cú pháp type(<tên biến>)
+type(h)
+type(hi)
