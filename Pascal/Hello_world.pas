5802306c7306c140a4a05a50df0433467eb6394d
HienTai <tai.nguyenraphael@hcmut.edu.vn>
2021-09-20 18:05:59 +0700

    commit

--- Pascal/Hello_world.pas
+++ Pascal/Hello_world.pas
@@ -6,7 +6,7 @@ begin
     assign(fi, 'lol.inp');reset(fi);
     assign(fo, 'lol.out');rewrite(fo);
     readln(fi,a);
-    writeln(fo,'10*A=',a*10);
+    writeln(fo,'10*A=',A*10);
     close(fo);
     close(fi);
     readln;
