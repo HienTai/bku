3ee3ad1be510844391cafa4270b6504ec0a850d8
HienTai <tai.nguyenraphael@hcmut.edu.vn>
2021-09-20 18:03:49 +0700

    commit

---
+++ Pascal/Hello_world.pas
@@ -0,0 +1,13 @@
+uses crt;
+var fi,fo: text;
+     a: integer;
+begin
+    clrscr;
+    assign(fi, 'lol.inp');reset(fi);
+    assign(fo, 'lol.out');rewrite(fo);
+    readln(fi,a);
+    writeln(fo,'10*A=',a*10);
+    close(fo);
+    close(fi);
+    readln;
+end.
